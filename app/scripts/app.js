72714297fcb3d55bf9bcab9288d6ed197c5b2bcc
Georg Heiler <georg.kf.heiler@gmail.com>
2014-03-06 16:04:23 +0100

    d3 working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('d3xmpApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-      
-    $locationProvider.html5Mode(true);
-  });
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'partials/main',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        $locationProvider.html5Mode(true);
+    });
